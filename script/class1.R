832174d5297e304b132961a5be87d5b3c04534c5
chen2369 <36851709+chen2369@users.noreply.github.com>
2020-04-14 15:02:57 +0800

    #

--- script/class1.R
+++ script/class1.R
@@ -1,16 +1,19 @@
+# load library
 library(tidyverse)
 library(R6)
 
+# Market Class
 Market <- R6Class("Market",
           public = list(
-            total = NA,
-            price = NULL,
-            dprice = NULL,
-            trial = 1,
-            mrow = NA,
-            mcol = NA,
-            change = NA,
-            board = matrix(c(0.05, 0.03, 0, 0.03, 0, -0.03, 0, -0.03, -0.05), nrow = 3, ncol = 3),
+            total = NA,     #?????????
+            price = NULL,   #??????
+            dprice = NULL,  #????????????
+            trial = 1,      #???????????????
+            mrow = NA,      #P1??????
+            mcol = NA,      #P2??????
+            change = NA,    #?????????
+            board = matrix(c(0.05, 0.03, 0, 0.03, 0, -0.03, 0, -0.03, -0.05), nrow = 3, ncol = 3),  #??????????????? 
+            # ???????????????
             initialize = function(total){
               stopifnot(is.numeric(total), length(total) == 1)
               self$total <- total
@@ -19,12 +22,14 @@ Market <- R6Class("Market",
               self$price[1] <- 100
               self$dprice[1] <- 0
             },
+            # ????????????
             condition = function(con){
               self$board = switch (con,
               Balance={matrix(c(0.05, 0.03, 0, 0.03, 0, -0.03, 0, -0.03, -0.05), nrow = 3, ncol = 3)},
               Bubble={matrix(c(0.10, 0.06, 0, 0.06, 0, -0.03, 0, -0.03, -0.05), nrow = 3, ncol = 3)},
               Burst={matrix(c(0.05, 0.03, 0, 0.03, 0, -0.06, 0, -0.06, -0.10), nrow = 3, ncol = 3)})
             },
+            # ????????????
             game = function(p1act, p2act){
               self$mrow = switch(p1act, B={1}, N={2}, S={3})
               self$mcol = switch(p2act, B={1}, N={2}, S={3})
@@ -36,16 +41,18 @@ Market <- R6Class("Market",
             lock_objects = F
           ))
 
+#Player Class
 Player <- R6Class("Player",
           public = list(
-            type = NULL,
-            cash = NULL,
-            stock = NULL,
-            value = NULL,
-            asset = NULL,
-            decision = NULL,
-            total = NA,
-            prob = NULL,
+            type = NULL,     #????????????
+            cash = NULL,     #??????
+            stock = NULL,    #??????
+            value = NULL,    #????????????
+            asset = NULL,    #?????????
+            decision = NULL, #??????
+            total = NA,      #????????????
+            prob = NULL,     #??????
+            # ???????????????
             initialize = function(cash,stock,type,total){
               stopifnot(is.numeric(cash), length(cash) == 1)
               stopifnot(is.numeric(stock), length(stock) == 1)
@@ -63,12 +70,14 @@ Player <- R6Class("Player",
               self$value[1] = stock * 100
               self$asset[1] = cash + stock * 100
             },
+            # ????????????
             decide = function(){
+              # ????????????????????????
               if(Market$dprice[Market$trial] > 0){
                 Prob = switch(self$type,
                       Herd={rmultinom(1, size = 1, prob = c(0.7,0.2,0.1))},
                       Inversive={rmultinom(1, size = 1, prob = c(0.1,0.2,0.7))},
-                      Hedge={rmultinom(1, size = 1, prob = c(0.65,0.3,0.05))}) 
+                      Hedge={rmultinom(1, size = 1, prob = c(0.05,0.3,0.65))}) 
               } else if(Market$dprice[Market$trial] == 0){
                 Prob = switch(self$type,
                       Herd={rmultinom(1, size = 1, prob = c(1/3,1/3,1/3))},
@@ -80,6 +89,7 @@ Player <- R6Class("Player",
                       Inversive={rmultinom(1, size = 1, prob = c(0.7,0.2,0.1))},
                       Hedge={rmultinom(1, size = 1, prob = c(0.3,0.65,0.05))}) 
               }
+              # ??????????????????
               if(Prob[1,1]){
                 if(self$cash[Market$trial] > Market$price[Market$trial]){
                   self$decision[Market$trial] = "B"
@@ -108,6 +118,7 @@ Player <- R6Class("Player",
                 }
               }
             },
+            # ????????????
             ending = function(){
               self$value[Market$trial] = self$stock[Market$trial-1] * Market$price[Market$trial-1]
               self$asset[Market$trial] = self$cash[Market$trial-1] + self$value[Market$trial-1]
--- script/simulation.R
+++ script/simulation.R
@@ -1,8 +1,8 @@
-source('class1.R', echo=TRUE)
+source('class1.R', echo=TRUE)     # source
 
-Market <- Market$new(total = 100)
-P1 <- Player$new(10000,10,"Herd",100)
-P2 <- Player$new(10000,10,"Hedge",100)
+Market <- Market$new(total = 100) # Market
+P1 <- Player$new(10000,10,"Hedge",100)   #P1
+P2 <- Player$new(10000,10,"Hedge",100)  #P2
 for (i in 1:Market$total) {
   P1$decide()
   P2$decide()
@@ -18,8 +18,19 @@ for (i in 1:Market$total) {
   P2$ending()
 }
 # 
+cash <- data.frame(rep(1:101),P1$cash, P2$cash)
+asset <- data.frame(rep(1:101),P1$asset, P2$asset)
+ggplot(cash) +
+  geom_line(aes(rep.1.101.,P1$cash), color = "red") +
+  geom_line(aes(rep.1.101.,P2$cash), color = "blue")
+ggplot(asset) +
+  geom_line(aes(rep.1.101.,P1$asset), color = "red") +
+  geom_line(aes(rep.1.101.,P2$asset), color = "blue")
+
 P1$decision
-P2$cash
-P2$decision
 P1$cash
+P1$stock
+P2$decision
+P2$cash
+P2$stock
 Market$price
